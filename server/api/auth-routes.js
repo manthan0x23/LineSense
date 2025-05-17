344ce6c443bba833898baba118fead9f44770eb2
Manthan Sharma <manthan0x23@gmail.com>
2025-05-17 14:57:14 +0530

    cookie-fix

--- server/api/auth-routes.js
+++ server/api/auth-routes.js
@@ -55,7 +55,6 @@ authRouter.post("/register", async (req, res) => {
   );
 
   res.cookie("token", token, {
-    httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
     maxAge: 60 * 60 * 1000,
@@ -92,7 +91,6 @@ authRouter.post("/login", async (req, res) => {
   );
 
   res.cookie("token", token, {
-    httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
     maxAge: 60 * 60 * 1000,
